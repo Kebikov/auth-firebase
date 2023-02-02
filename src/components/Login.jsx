4cd8c9d76d8d08257016628531e228e354f040f8
Kebikov Evgenii <kebikovgen@gmail.com>
2023-02-02 19:40:42 +0300

    first commit

---
+++ src/components/Login.jsx
@@ -0,0 +1,39 @@
+//* Воход существующих пользователей 
+import {useDispatch} from 'react-redux';
+import {useHistory} from 'react-router-dom';
+
+import {Form} from './Form';
+import {setUser} from 'store/slices/userSlice';
+
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+// импорт функций из firebase
+
+const Login = () => {
+    const dispatch = useDispatch();
+    const {push} = useHistory();
+    
+    const handleLogin = (email, password) => {
+        const auth = getAuth();
+//...создаем запрос к firebase с данными, который вернет нам promis c информацией о пользователе
+        signInWithEmailAndPassword(auth, email, password)
+            .then(({user}) => {
+                console.log(user);
+                dispatch(setUser({
+                    email: user.email,
+                    id: user.uid,
+                    token: user.accessToken,
+                }));
+                push('/');
+            })
+            .catch(() => alert('Invalid user!'))
+    }
+
+    return (
+        <Form
+            title="sign in"
+            handleClick={handleLogin}
+        />
+    )
+}
+
+export {Login}
